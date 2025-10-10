a22fcc5dc96d4e6888bd322f4ba68b2a24c0deda
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-10-10 09:54:00 +0200

    add toast on variable deletion. make project name not nullable for variables

--- packages/frontend/editor-ui/src/components/Projects/ProjectTabs.vue
+++ packages/frontend/editor-ui/src/components/Projects/ProjectTabs.vue
@@ -11,6 +11,7 @@ import { usePostHog } from '@/stores/posthog.store';
 import { useProjectsStore } from '@/stores/projects.store';
 
 import { N8nTabs } from '@n8n/design-system';
+import { ProjectTypes } from '@/types/projects.types';
 type Props = {
 	showSettings?: boolean;
 	showExecutions?: boolean;
@@ -47,7 +48,7 @@ const projectId = computed(() => {
 		: route?.params?.projectId;
 });
 
-const isTeamProject = computed(() => projectStore.currentProject?.type === 'team');
+const isTeamProject = computed(() => projectStore.currentProject?.type === ProjectTypes.Team);
 
 const getRouteConfigs = () => {
 	// For project pages
